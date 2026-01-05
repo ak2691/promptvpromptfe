c0fc68dffe0602f1b02e326404ca3ae9f4eec205
ak2691 <118644895+ak2691@users.noreply.github.com>
2026-01-04 20:52:50 -0600

    fresh

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
-import Flashcard from './components/flashcard/Flashcard';
-import DeckTest from './components/test/DeckTest';
+
 import Home from './components/ui/Home';
 
 function App() {
@@ -13,8 +12,7 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/flashcard" element={<Flashcard />} />
-        <Route path="/decktest" element={<DeckTest />} />
+
         <Route path="/" element={<Home />} />
       </Routes>
 
